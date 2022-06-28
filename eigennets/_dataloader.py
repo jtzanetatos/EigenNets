aa249e21ffca9dca5409e01add5bce206d34aa19
jtzanetatos <58182829+jtzanetatos@users.noreply.github.com>
2022-06-28 22:29:56 +0300

    Implemented dataloader function
    - While batch size can be operational, pytorch:dataloader appends an extra dim.
    - Could either edit the training loop or the input of the dataloader

--- eigennets/_dataloader.py
+++ eigennets/_dataloader.py
@@ -11,15 +11,22 @@ from torch import zeros, tensor
 from torch.utils.data import DataLoader
 from warnings import warn
 from numpy import arange
-from torchvision.io import read_image
-from torchvision.transforms import Compose, ToTensor
+from PIL import Image
+from torchvision.transforms import Compose, ToTensor, Normalize
 
 class _image_dataset(DataLoader):
     
     def __init__(self,
                  train: list,
                  labels: list,
-                 t_transforms,
+                 channels: int=3,
+                 t_transforms: Compose = Compose([ToTensor(),
+                                                  Normalize(
+                                                      mean=[0.485, 0.456, 0.406],
+                                                      std=[0.229, 0.224, 0.225]
+                                                      )
+                                                  ]
+                                                 ),
                  batch_size: int = 1,
                  target_size: list = None,
                  l_transform: Compose = Compose([ToTensor()]),
@@ -28,18 +35,19 @@ class _image_dataset(DataLoader):
             warn("Batch size cannot be less than 1, setting size to 1.")
             batch_size = 1
         
-        super().__init__()
         self.input = train
         self.labels = labels
+        self.channels = channels
         self.t_transforms = t_transforms
         self.batch_size = batch_size
         self.target_size = target_size
         self.indexes = arange(len(train))
         self.l_transform = l_transform
         
+        
     def __len__(self):
         
-        return len(self.train) // self.batch_size
+        return len(self.input) // self.batch_size
     
     def __getitem__(self, index: int) -> Union[tensor, tensor]:
         """Returns tuple (input, target) correspond to batch #idx."""
@@ -47,30 +55,38 @@ class _image_dataset(DataLoader):
         idx = self.indexes[shuffidx * self.batch_size:
                            (shuffidx + 1) * self.batch_size]
         
-        batch_input_img_paths = [self.train[k] for k in idx]
+        batch_input_img_paths = [self.input[k] for k in idx]
         batch_target_img_paths = [self.labels[k] for k in idx]
         
         # No target shape entered
         if self.target_size is None:
             # Batch size 1
             if self.batch_size == 1:
-                temp = read_image(batch_input_img_paths)
+                temp = Image.open(batch_input_img_paths)
             else:
-                temp = read_image(batch_input_img_paths[0])
+                temp = Image.open(batch_input_img_paths[0])
             self.target_size = temp.shape
         # Inputs and labels preallocation
-        x = zeros(((self.batch_size) + self.target_size), dtype='uint8')
-        y = zeros(((self.batch_size) + self.target_size), dtype='uint8')
+        # x = zeros(((self.batch_size,) + (self.channels,) + (self.target_size)))
+        # y = zeros(((self.batch_size,) + (self.channels,) + (self.target_size)))
+        x = zeros(((self.channels,) + (self.target_size)))
+        y = zeros(((self.channels,) + (self.target_size)))
         
         # Iterate over batch
-        for i, in_path, l_path in enumerate(zip(batch_input_img_paths,
+        for i, (in_path, l_path) in enumerate(zip(batch_input_img_paths,
                                                 batch_target_img_paths)):
             # Load training sample & corresponding label
-            x[i] = read_image(in_path)
-            y[i] = read_image(l_path)
+            in_img = Image.open(in_path)
+            label_img = Image.open(l_path)
+            
+            if self.target_size is not None:
+                in_img = in_img.resize(self.target_size, Image.LANCZOS)
+                label_img = label_img.resize(self.target_size, Image.LANCZOS)
             
             # Apply transformations
-            x[i] = self.t_transforms(x[i])
-            y[i] = self.l_transform(y[i])
+            # x[i] = self.t_transforms(in_img)
+            # y[i] = self.l_transform(label_img)
+            x = self.t_transforms(in_img)
+            y = self.l_transform(label_img)
         # Return training set & label(s)
         return x, y
--- eigennets/_mavecodd.py
+++ eigennets/_mavecodd.py
@@ -10,6 +10,7 @@ from os import listdir
 from os.path import join, abspath
 from typing import TypedDict, List
 from warnings import warn
+from numpy import asarray
 
 class MaVeCoDD_dtype(TypedDict):
     split: str
@@ -21,6 +22,17 @@ def _split_safeguard(obj: object) -> None:
         warn("Invalid key entered, default behaviour is mixed data mining",
              category=SyntaxWarning)
         obj.split_type = "mixed"
+
+def _list2array(dirs: list) -> None:
+    # Initialize new list
+    dataset = []
+    
+    # Iterate over dirs
+    for path in dirs:
+        dataset.append(asarray(path))
+    
+    return dataset
+
 
 class MaVeCoDD:
     
@@ -45,6 +57,7 @@ class MaVeCoDD:
         
     # TODO: Parallel access to directories
     def _set_subdir_paths(self) -> List:
+        
         # Initialize dataset
         dataset = [[], []]
         # Mixed case
@@ -68,6 +81,9 @@ class MaVeCoDD:
         # Sort paths to ensure proper sequence
         for i in range(len(dataset)):
             dataset[i].sort()
+        
+        # Convert to array
+        dataset = _list2array(dataset)
         # Return dataset
         return dataset
     
@@ -80,3 +96,40 @@ class MaVeCoDD:
     @property
     def get_dataset(self):
         return self.dataset
+
+
+
+# Deprecated --------------------------------------------------------------- #
+# -------------------------------------------------------------------------- #
+# def _merge_mixed(dirs: list) -> None:
+#     # Size of dataset
+#     d_size = len(dirs[0])
+#     # Initialize list of train/label arrays
+#     dataset = [zeros(d_size, dtype=str) for _ in range(2)]
+    
+#     # Sub-bucket indices
+#     l_idx = 0
+#     t_idx = 0
+    
+#     # Iterate over dataset
+#     for i, dir_imgs in enumerate(dirs):
+#         # Label bucket
+#         if dir_imgs[0].find("label") != -1:
+#             # First sub-bucket
+#             if l_idx == 0:
+#                 dataset[1][:d_size] = asarray(dir_imgs).copy()
+#                 # Update index
+#                 l_idx += 1
+#             else:
+#                 dataset[1][d_size:] = asarray(dir_imgs).copy()
+#         # Train bucket
+#         else:
+#             # First sub-bucket
+#             if t_idx == 0:
+#                 dataset[0][:d_size] = asarray(dir_imgs).copy()
+#                 # Update index
+#                 t_idx += 1
+#             else:
+#                 dataset[0][d_size:] = asarray(dir_imgs).copy()
+#     # Return merged dataset
+#     return dataset
