f97b35d0e8a5c23a5749ae35c70d8a9abf1f4d31
jtzanetatos <58182829+jtzanetatos@users.noreply.github.com>
2022-06-30 16:30:53 +0300

    Added cuda flags; capture metrics

--- main.py
+++ main.py
@@ -9,16 +9,17 @@ Created on Mon Jun 27 18:34:13 2022
 from eigennets import MaVeCoDD_dataset, MaVeCoDD
 from eigennets import segmentation_models as sm
 from sklearn.model_selection import KFold
-from segmentation_models_pytorch.losses import JaccardLoss
+# from segmentation_models_pytorch.losses import JaccardLoss
 from segmentation_models_pytorch.utils.metrics import IoU, Fscore
 from segmentation_models_pytorch.utils.train import TrainEpoch, ValidEpoch
 from torch.utils.data import DataLoader
+from torch.nn import BCELoss
 from torch.optim import Adam
-from torch import seed as tseed
+import torch
+from torch.backends.cudnn import deterministic, benchmark
 from torch import save as tsave
 from torch.cuda import is_available
 from typing import Union, Iterable
-from os.path import join
 
 def _init_dataset() -> Union[Iterable, Iterable]:
     
@@ -31,7 +32,10 @@ def _init_dataset() -> Union[Iterable, Iterable]:
     return train, label
 
 # Torch seed
-tseed(42)
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+deterministic = True
+benchmark = False
 
 # Use gpu if available
 DEVICE = 'cuda' if is_available() else 'cpu'
@@ -39,9 +43,9 @@ DEVICE = 'cuda' if is_available() else 'cpu'
 # Train & label samples
 train, label = _init_dataset()
 # Image resize
-target_size = (320, 320)
+target_size = (640, 640)
 # Batch size
-batch_size = 2
+batch_size = 1
 
 # Initialize model parameters - Segmentation models
 model_params = {"encoder_name" : "resnet18",
@@ -53,19 +57,19 @@ model_params = {"encoder_name" : "resnet18",
                 }
 # Save path for models
 # TODO: Make a dynamic version as to not to re-write?
-save_path = '/saved_models/segmentation_models/'
+save_path = 'saved_models/segmentation_models/'
 # Epochs
 epochs = 20
 
 # Number of k-fold splits
-n_splits = 5
+n_splits = 3
 
 # Loss list
-vk_loss = [ [] for _ in range(n_splits)]
-tk_loss = vk_loss.copy()
+vk_loss = [[] for _ in range(n_splits)]
+tk_loss = [[] for _ in range(n_splits)]
 # Metrics
-tk_metris = tk_loss.copy()
-vk_metris = tk_loss.copy()
+tk_metris = [[] for _ in range(n_splits)]
+vk_metris = [[] for _ in range(n_splits)]
 
 # Initialize K-Fold
 kf = KFold(n_splits=n_splits, shuffle=True, random_state=42)
@@ -102,7 +106,8 @@ for i, (train_idx, test_idx) in enumerate(kf.split(X=train, y=label)):
     model.unetplusplus()
     
     # Loss
-    loss = JaccardLoss(mode='binary')
+    # loss = JaccardLoss(mode='binary')
+    loss = BCELoss()
     
     # Metrics
     metrics = [IoU(threshold=0.5),
@@ -129,19 +134,19 @@ for i, (train_idx, test_idx) in enumerate(kf.split(X=train, y=label)):
                             )
     # Train model
     for epoch in range(epochs):
-        print('\nEpoch: {}'.format(i))
+        print(f"\nK-Fold split {i+1} Epoch: {epoch+1}")
         train_logs = train_epoch.run(train_loader)
         valid_logs = valid_epoch.run(valid_loader)
         
         # Capture metrics
-        vk_loss[i].append(valid_logs['JaccardLoss'])
-        tk_loss[i].append(train_logs['JaccardLoss'])
-        tk_metris[i].append(train_logs['iou_score'], train_logs['fscore'])
+        vk_loss[i].append(valid_logs['BCELoss'])
+        tk_loss[i].append(train_logs['BCELoss'])
+        tk_metris[i].append([train_logs['iou_score'], train_logs['fscore']])
         vk_metris[i].append([valid_logs['iou_score'], valid_logs['fscore']])
         
         # Save best model weights
         if max_score < valid_logs['iou_score']:
             max_score = valid_logs['iou_score']
-            tsave(model, join(save_path, f"k_split{i+1}_{vk_loss[i][epoch]}_iou{max_score}_model.pth"))
+            tsave(model, f"{save_path}{i+1}_best_model.pth")
             print('Model saved!')
 
